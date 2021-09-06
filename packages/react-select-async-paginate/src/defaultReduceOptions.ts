27165a667e435228774f85a2aa6535b168eb9c29
jssaints <info@jssaints.com>
2021-09-06 11:12:16 +0530

    latest file deleted

--- packages/react-select-async-paginate/src/defaultReduceOptions.ts
+++
@@ -1,8 +0,0 @@
-import type {
-  ReduceOptions,
-} from './types';
-
-export const defaultReduceOptions: ReduceOptions<any, any> = (
-  prevOptions,
-  loadedOptions,
-) => [...prevOptions, ...loadedOptions];
